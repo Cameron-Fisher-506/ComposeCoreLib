2661a7289db5ae716e542ce128fba095d52a11de
Cameron-Fisher-506 <fishercameron506@gmail.com>
2025-11-12 14:59:03 +0200

    Updated dependency versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "za.co.composecorelib"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "za.co.composecorelib"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
